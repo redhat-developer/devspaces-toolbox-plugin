2d96d9644fd624089d7d2a25fbaaa5a8e7180107
Artem Zatsarynnyi <azatsary@redhat.com>
2026-05-21 13:37:06 +0200

    Bump version from 0.0.4 to 0.0.5

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 // Note, the `group` value is used as a provider ID
 // when handling the URLs like `jetbrains://gateway/provider.ID`
 group = "com.redhat.devtools.toolbox"
-version = "0.0.4"
+version = "0.0.5"
 
 extra["vendor"] = "Red-Hat"
 
